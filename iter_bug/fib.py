96a0978419e0aca3f5809384612a52acad210fdf
Jesus Riverajimenez <riveraji@arctic.cse.msu.edu>
2013-01-21 23:19:04 -0500

    fixed the bug

--- iter_bug/fib.py
+++ iter_bug/fib.py
@@ -1,7 +1,3 @@
-# this is an implementation of the Fibonacci series using
-# Python's iterator functionality.  Here, 'fib' is a class that
-# obeys the iterator protocol.
-
 class fib(object):
     def __init__(self):
         self.last_1 = 1
@@ -12,5 +8,6 @@ class fib(object):
 
     def next(self):
         next_fib = self.last_1 + self.last_2
-        self.last_1, self.last2 = self.last_2, next_fib
+        self.last_1, self.last_2 = self.last_2, next_fib
         return next_fib
+
--- iter_bug/test.py
+++ iter_bug/test.py
@@ -3,7 +3,8 @@ import fib
 def test1():
     f = fib.fib()
     i = iter(f)
-    assert i.next() == 2
+
+    assert i.next() ==2
 
 def test2():
     f = fib.fib()
@@ -11,7 +12,7 @@ def test2():
     i.next()
     assert i.next() == 3
 
-def test2():
+def test3():
     f = fib.fib()
     i = iter(f)
     i.next()
